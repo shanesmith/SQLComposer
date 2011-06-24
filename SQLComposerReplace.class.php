e625a930e8591364bed29cec8bab6b9b30273c09
Shane Smith <cap.webmanager@gmail.com>
2011-06-24 10:41:20 -0400

    PHPDoc for SQLComposerReplace

--- SQLComposerReplace.class.php
+++ SQLComposerReplace.class.php
@@ -1,33 +1,82 @@
 <?php
 require_once 'SQLComposer.class.php';
 
+/**
+ * SQLComposerReplace
+ *
+ * A REPLACE query
+ */
 class SQLComposerReplace extends SQLComposerBase {
 
+	/**
+	 * To create an REPLACE INTO ... SELECT ...
+	 *
+	 * @var SQLComposerSelect
+	 */
 	protected $select;
 
+	/*******************
+	 **  CONSTRUCTOR  **
+	 *******************/
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $table
+	 */
 	public function __construct($table = null) {
 		if (isset($table)) $this->into($table);
 	}
 
+	/**
+	 * REPLACE INTO
+	 *
+	 * @param string $table
+	 * @return SQLComposerReplace
+	 */
 	public function replace_into($table) {
 		return $this->into($table);
 	}
 
+	/**
+	 * REPLACE INTO
+	 *
+	 * @param string $table
+	 * @return SQLComposerReplace
+	 */
 	public function into($table) {
 		$this->add_table($table);
 		return $this;
 	}
 
+	/**
+	 * Set the columns for REPLACE INTO table (col1, col2, ...)
+	 *
+	 * @param array $columns
+	 * @return SQLComposerReplace
+	 */
 	public function columns(array $columns) {
 		$this->columns = $columns;
 		return $this;
 	}
 
-	public function add_column($column) {
-		$this->columns[] = $column;
-		return $this;
-	}
-
+	/**
+	 * Provide a set of values to be replaced.
+	 *
+	 * If the columns are not yet set and an associative array is passed,
+	 * the array keys will be used as the columns.
+	 *
+	 * ex:
+	 *  SQLComposer::replace_into('table')->values(array( 'id' => '25', 'name' => 'joe', 'fav_color' => 'green' ));
+	 *
+	 * will result in
+	 *
+	 *  REPLACE INTO table (id, name, fav_color) VALUES (25, 'joe', 'green')
+	 *
+	 * @param array $values
+	 * @param string $mysqli_types
+	 * @return SQLComposerReplace
+	 */
 	public function values(array $values, $mysqli_types = "") {
 		if (isset($this->select)) throw new SQLComposerException("Cannot use 'REPLACE INTO ... VALUES' when a SELECT is already set!");
 
@@ -38,6 +87,14 @@ class SQLComposerReplace extends SQLComposerBase {
 		return $this->_add_params('values', array( $values ), $mysqli_types);
 	}
 
+	/**
+	 * Return a SQLComposerSelect object to be used in a query of the type REPLACE INTO ... SELECT ...
+	 *
+	 * @param string $select
+	 * @param array $params
+	 * @param string $mysqli_types
+	 * @return SQLComposerSelect
+	 */
 	public function select($select = null, array $params = null, $mysqli_types = "") {
 		if (isset($this->params['values'])) throw new SQLComposerException("Cannot use 'REPLACE INTO ... SELECT' when values are already set!");
 
@@ -48,6 +105,16 @@ class SQLComposerReplace extends SQLComposerBase {
 		return $this->select->select($select, $params, $mysqli_types);
 	}
 
+
+	/*****************
+	 **  RENDERING  **
+	 *****************/
+
+	/**
+	 * Render the REPLACE query
+	 *
+	 * @return string
+	 */
 	public function render() {
 		$table = $this->tables[0];
 
@@ -68,6 +135,11 @@ class SQLComposerReplace extends SQLComposerBase {
 		return "REPLACE INTO {$table} {$columns} {$values}";
 	}
 
+	/**
+	 * Get the parameters array
+	 *
+	 * @return array
+	 */
 	public function getParams() {
 
 		if (isset($this->select)) {
