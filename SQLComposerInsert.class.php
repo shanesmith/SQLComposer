7901331f0979dd441b38ad365f18f8d059bff574
Shane Smith <cap.webmanager@gmail.com>
2011-06-24 10:17:10 -0400

    PHPDoc for SQLComposerInsert

--- SQLComposerInsert.class.php
+++ SQLComposerInsert.class.php
@@ -1,42 +1,113 @@
 <?php
 require_once 'SQLComposer.class.php';
 
+/**
+ * SQLComposerInsert
+ *
+ * An INSERT query
+ */
 class SQLComposerInsert extends SQLComposerBase {
 
+	/**
+	 * IGNORE
+	 *
+	 * @var bool
+	 */
 	protected $ignore = false;
 
+	/**
+	 * To create an INSERT INTO ... SELECT ...
+	 *
+	 * @var SQLComposerSelect
+	 */
 	protected $select;
 
+	/**
+	 * ON DUPLICATE KEY UPDATE
+	 *
+	 * @var array
+	 */
 	protected $on_duplicate = array( );
 
+
+	/*******************
+	 **  CONSTRUCTOR  **
+	 *******************/
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $table
+	 */
 	public function __construct($table = null) {
 		if (isset($table)) $this->into($table);
 	}
 
+
+	/***************
+	 **  METHODS  **
+	 ***************/
+
+	/**
+	 * INSERT INTO
+	 *
+	 * @param string $table
+	 * @return SQLComposerInsert
+	 */
 	public function insert_into($table) {
 		return $this->into($table);
 	}
 
+	/**
+	 * INSERT INTO
+	 *
+	 * @param string $table
+	 * @return SQLComposerInsert
+	 */
 	public function into($table) {
 		$this->add_table($table);
 		return $this;
 	}
 
+	/**
+	 * IGNORE
+	 *
+	 * @param bool $ignore
+	 * @return SQLComposerInsert
+	 */
 	public function ignore($ignore = true) {
 		$this->ignore = $ignore;
 		return $this;
 	}
 
+	/**
+	 * Set the columns for INSERT INTO table (col1, col2, ...)
+	 *
+	 * @param array $columns
+	 * @return SQLComposerInsert
+	 */
 	public function columns(array $columns) {
 		$this->columns = $columns;
 		return $this;
 	}
 
-	public function add_column($column) {
-		$this->columns[] = $column;
-		return $this;
-	}
-
+	/**
+	 * Provide a set of values to be inserted.
+	 *
+	 * If the columns are not yet set and an associative array is passed,
+	 * the array keys will be used as the columns.
+	 *
+	 * ex:
+	 *  SQLComposer::insert_into('table')->values(array( 'id' => '25', 'name' => 'joe', 'fav_color' => 'green' ));
+	 *
+	 * will result in
+	 *
+	 *  INSERT INTO table (id, name, fav_color) VALUES (25, 'joe', 'green')
+	 *
+	 * @param array $values
+	 * @param string $mysqli_types
+	 * @return SQLComposerInsert
+	 */
 	public function values(array $values, $mysqli_types = "") {
 		if (isset($this->select)) throw new SQLComposerException("Cannot use 'INSERT INTO ... VALUES' when a SELECT is already set!");
 
@@ -47,6 +118,14 @@ class SQLComposerInsert extends SQLComposerBase {
 		return $this->_add_params('values', array( $values ), $mysqli_types);
 	}
 
+	/**
+	 * Return a SQLComposerSelect object to be used in a query of the type INSERT INTO ... SELECT ...
+	 *
+	 * @param string $select
+	 * @param array $params
+	 * @param string $mysqli_types
+	 * @return SQLComposerSelect
+	 */
 	public function select($select = null, array $params = null, $mysqli_types = "") {
 		if (isset($this->params['values'])) throw new SQLComposerException("Cannot use 'INSERT INTO ... SELECT' when values are already set!");
 
@@ -54,15 +133,37 @@ class SQLComposerInsert extends SQLComposerBase {
 			$this->select = SQLComposer::select();
 		}
 
-		return $this->select->select($select, $params, $mysqli_types);
+		if (isset($select)) {
+			$this->select->select($select, $params, $mysqli_types);
+		}
+
+		return $this->select;
 	}
 
+	/**
+	 * ON DUPLICATE KEY UPDATE
+	 *
+	 * @param array $update
+	 * @param array $params
+	 * @param string $mysqli_types
+	 * @return SQLComposerInsert
+	 */
 	public function on_duplicate(array $update, array $params = null, $mysqli_types = "") {
 		$this->on_duplicate = array_merge($this->on_duplicate, $update);
 		$this->_add_params('on_duplicate', $params, $mysqli_types);
 		return $this;
 	}
 
+
+	/*****************
+	 **  RENDERING  **
+	 *****************/
+
+	/**
+	 * Render the INSERT query
+	 *
+	 * @return string
+	 */
 	public function render() {
 		$table = $this->tables[0];
 
@@ -88,6 +189,11 @@ class SQLComposerInsert extends SQLComposerBase {
 		return "INSERT {$ignore} INTO {$table} {$columns} {$values} {$on_duplicate}";
 	}
 
+	/**
+	 * Get the parameters array
+	 *
+	 * @return array
+	 */
 	public function getParams() {
 
 		if (isset($this->select)) {
