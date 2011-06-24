61a0a9681c268d2abc4fc4252cbe6d3e3d3dd313
Shane Smith <cap.webmanager@gmail.com>
2011-06-24 10:49:33 -0400

    PHPDoc for SLQComposerUpdate

--- SQLComposerUpdate.class.php
+++ SQLComposerUpdate.class.php
@@ -1,22 +1,93 @@
 <?php
 require_once 'SQLComposer.class.php';
 
-
+/**
+ * SQLComposerUpdate
+ *
+ * An UPDATE query
+ */
 class SQLComposerUpdate extends SQLComposerWhere {
 
+	/**
+	 * SET
+	 *
+	 * @var array
+	 */
 	protected $set = array( );
+
+	/**
+	 * ORDER BY
+	 *
+	 * @var array
+	 */
 	protected $order_by = array( );
+
+	/**
+	 * LIMIT
+	 *
+	 * @var int
+	 */
 	protected $limit = null;
+
+	/**
+	 * IGNORE
+	 *
+	 * @var bool
+	 */
 	protected $ignore = false;
 
+
+	/*******************
+	 **  CONSTRUCTOR  **
+	 *******************/
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $table
+	 */
 	public function __construct($table = null) {
 		if (isset($table)) $this->update($table);
 	}
 
+
+	/**************
+	 **  METHODS **
+	 **************/
+
+	/**
+	 * UPDATE
+	 *
+	 * @param string $table
+	 * @return SQLComposerUpdate
+	 */
 	public function update($table) {
 		$this->add_table($table);
+		return $this;
 	}
 
+	/**
+	 * SET
+	 *
+	 * Can either be a string of the form "col=?"
+	 * or an array with columns as keys and corresponding values as parameters.
+	 *
+	 * ex:
+	 *  SQLComposer::update('table')->set(array( 'name' => 'john', 'fav_color' => 'blue' ))->where("id=?", array(25));
+	 *
+	 * will result in
+	 *
+	 *  UPDATE table SET name=?, fav_color=? WHERE id=?
+	 *
+	 * and parameters
+	 *
+	 *  array('john', 'blue', 25)
+	 *
+	 * @param string|array $set
+	 * @param array $params
+	 * @param null $mysqli_types
+	 * @return SQLComposerUpdate
+	 */
 	public function set($set, array $params = null, $mysqli_types = null) {
 		if (is_array($set)) {
 			foreach ($set as $col => $val) $this->set[] = "{$col}=?";
@@ -28,21 +99,49 @@ class SQLComposerUpdate extends SQLComposerWhere {
 		return $this;
 	}
 
+	/**
+	 * ORDER BY
+	 *
+	 * @param string $order_by
+	 * @return SQLComposerUpdate
+	 */
 	public function order_by($order_by) {
 		$this->order_by[] = $order_by;
 		return $this;
 	}
 
+	/**
+	 * LIMIT
+	 *
+	 * @param int $limit
+	 * @return SQLComposerUpdate
+	 */
 	public function limit($limit) {
 		$this->limit = $limit;
 		return $this;
 	}
 
+	/**
+	 * IGNORE
+	 *
+	 * @param bool $ignore
+	 * @return SQLComposerUpdate
+	 */
 	public function ignore($ignore = true) {
 		$this->ignore = $ignore;
 		return $this;
 	}
 
+
+	/****************
+	 ** RENDERING  **
+	 ****************/
+
+	/**
+	 * Render the SQL query
+	 *
+	 * @return string
+	 */
 	public function render() {
 		$ignore = $this->ignore ? "IGNORE" : "";
 
@@ -59,6 +158,11 @@ class SQLComposerUpdate extends SQLComposerWhere {
 		return "UPDATE {$ignore} {$tables} {$set} {$where} {$order_by} {$limit}";
 	}
 
+	/**
+	 * Get the parameters
+	 *
+	 * @return array
+	 */
 	public function getParams() {
 		return $this->_get_params("set", "where");
 	}
